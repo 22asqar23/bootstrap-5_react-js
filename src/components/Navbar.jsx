ae146c6ed16bd97da8a65a4d93372bc6bdf95119
Asqarjon <22asqar23@gmail.com>
2020-10-10 00:49:41 +0500

    Commit 2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="container">
+            <h4 className="text-center">Hello</h4>
+        </div>
+    )
+}
+
+export default Navbar;
