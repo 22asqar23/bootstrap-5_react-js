7cc8ef06a31c4180c29569807e36fb0174fcbfaa
Asqarjon <22asqar23@gmail.com>
2020-10-10 00:54:25 +0500

    commit 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle"
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
